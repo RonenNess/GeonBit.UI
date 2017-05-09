5421e1494a48ddf1ca6d7fed12d1a61c0b9df649
Ronen <RonenNess@gmail.com>
2017-05-09 21:50:45 +0200

    updated version and docs

--- GeonBit.UI/GeonBit.UI/UserInterface.cs
+++ GeonBit.UI/GeonBit.UI/UserInterface.cs
@@ -98,7 +98,7 @@ namespace GeonBit.UI
         }
 
         /// <summary>Current GeonBit.UI version identifier.</summary>
-        public const string VERSION = "2.0.2.0";
+        public const string VERSION = "2.0.2.1";
 
         // root panel that covers the entire screen and everything is added to it
         static RootPanel _root;
