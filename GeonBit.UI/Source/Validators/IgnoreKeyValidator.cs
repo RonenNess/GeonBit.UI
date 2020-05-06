89307b19e2b92f20a7f682c916f7ae1f4b9d00f4
Rixium <gamerpotty@gmail.com>
2020-05-06 19:43:03 +0100

    Add the ignore key validator

---
+++ GeonBit.UI/Source/Validators/IgnoreKeyValidator.cs
@@ -0,0 +1,33 @@
+﻿using GeonBit.UI.Entities.TextValidators;
+using Microsoft.Xna.Framework.Input;
+
+namespace GeonBit.UI.Validators
+{
+    /// <summary>
+    /// Ignores a given key
+    /// </summary>
+    public class IgnoreKeyValidator : ITextValidator
+    {
+        private readonly Keys _key;
+
+        /// <summary>
+        /// Ignores a given key
+        /// </summary>
+        /// <param name="key">The key to ignore</param>
+        public IgnoreKeyValidator(Keys key)
+        {
+            _key = key;
+        }
+
+        /// <summary>
+        /// Checks if the given key has been pressed, and if so, resets the text to its original text.
+        /// </summary>
+        public override bool ValidateText(ref string text, string oldText)
+        {
+            if (Keyboard.GetState().IsKeyDown(_key))
+                text = oldText;
+            
+            return base.ValidateText(ref text, oldText);
+        }
+    }
+}
