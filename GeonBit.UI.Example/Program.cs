607190024f269d123419e250d52636af99ba489e
David Fidge <dfidge@internode.on.net>
2021-09-26 12:29:37 +0930

    restructure projects to have a proper separation between data types lib and example project

---
+++ GeonBit.UI.Example/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace GeonBit.UI.Example
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new GeonBit.UI.Example.GeonBitUI_Examples())
+                game.Run();
+
+        }
+    }
+}
